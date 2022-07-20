fc0a6db330fc8f1ff7ca25467a7735b046c0f7a5
zeteticl <cougar.l@hotmail.com>
2022-07-20 15:58:44 +0800

    x

--- index copy.js
+++ index copy.js
@@ -12,6 +12,9 @@ class CollectionItem {
     get allItems() {
         return this.items
     }
+    get allItemsName() {
+        return this.items.map(item => item.name)
+    }
     // this could include summary stats like average score, etc. For simplicy, just the count for now
     get numberOfItems() {
         return this.items.length
@@ -43,24 +46,26 @@ function readFolderJson(folder) {
 
 
 function searchTargetKey(obj, keys) {
-    if (keys.includes('name') && keys.includes('ENG_name') && keys.includes('entries')) {
-        collection.newItem({ name: obj['name'], ENG_name: obj['ENG_name'], entries: obj['entries'] });
+    try {
+        if (keys.includes('name') && keys.includes('ENG_name') && keys.includes('entries')) {
+            collection.newItem({ name: obj['name'], ENG_name: obj['ENG_name'], entries: obj['entries'] });
+        }
+    } catch (error) {
+        return;
     }
+
 }
 
 function handlingObject(data) {
     const keys = findObjectKey(data);
-    //  console.log(keys);
     searchTargetKey(data, keys);//搜索所有object有沒有三個指定的key
     keys.forEach(key => {
         if (data[key] instanceof Array) {
             data[key].forEach(item => {
-                // console.log('item: ' + JSON.stringify(item));
                 handlingObject(item);
             })
         }
         if (data[key] instanceof Object) {
-            //  console.log('object: ' + JSON.stringify(data[key]));
             handlingObject(data[key]);
         }
     })
@@ -69,16 +74,14 @@ function handlingObject(data) {
 init()
 
 function init() {
-    //   const data = readFolderJson('data');    // read all json files
-    const datas = readFolderJson('data'); //讀取所有資料庫.JSON檔案
-
+    let datas = [];
+    datas.push(readFolderJson('./data'))
     datas.forEach(data => {
-        //  console.log('data: ' + JSON.stringify(data));
         handlingObject(data)
     })
 
     console.log(collection.numberOfItems)
-    //  console.log(JSON.stringify(collection.allItems))
+    //  console.log(JSON.stringify(collection.allItemsName))
 
 
 }
@@ -88,7 +91,7 @@ function findObjectKey(object) {
         const keys = Object.keys(object);
         return keys;
     } catch (error) {
-        return {}
+        return []
     }
 
 }
